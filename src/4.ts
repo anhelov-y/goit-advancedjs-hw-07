8c726886029d187308d8f5c5d50098c8a30668f7
anhelov-y <yuriy.anhelov@gmail.com>
2026-06-22 11:29:35 +0300

    UPD_1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} зайшов у будинок.`);
+    } else {
+      console.log('Двері зачинені! Зайти неможливо.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Двері успішно відчинено. Ласкаво просимо!');
+    } else {
+      console.log('Ключ не підходить! Двері залишаються зачиненими.');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
